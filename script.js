27bfda056da349b1b505d70f7b0de5715968ad09
Gsmeburg <gsm20090510@gmail.com>
2022-04-18 11:36:52 +0200

    ДЗ2

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const goods = [
-{ title: 'Shirt', price: 150 },
+{ title: 'Shirt',price: 150 },
 { title: 'Socks', price: 50 },
 { title: 'Jacket', price: 350 },
-{ price: 250 },
+{ title: 'Shoes', price: 250 },
 ];
-const renderGoodsItem = (title='Shoes', price) => {
+let sum = 0;
+function countSum(x) {
+for(i=0;i<goods.length;i++) {
+sum = sum + goods[i].price;
+}
+return sum;
+};
+
+const renderGoodsItem = (title, price) => {
 return `
 <div class="goods-item">
 <h2>${title}</h2>
@@ -17,3 +25,4 @@ let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
 document.querySelector('.goods-list').innerHTML = goodsList.join('');
 }
 renderGoodsList(goods);
+document.write('</br><b>Стоимость заказа: '+countSum(goods)+'руб.</b>');
